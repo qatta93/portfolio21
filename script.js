06903b97559899dab0b887cfd236fd90788a30ab
qatta93 <panasiuk.patrycja@gmail.com>
2021-09-06 17:02:55 +0200

    portfolio

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('anim'),
     rederer: 'svg',
@@ -11,10 +6,6 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'data.json'
 })
-
-// function ShowDiv() {
-//     document.getElementById("myDiv").style.display = "block";
-// }
 
 
 function ShowDiv() {
@@ -25,3 +16,39 @@ function ShowDiv() {
       x.style.display = "none";
     }
   }
+
+
+  //show projects details after clicking 
+  
+  const targetDiv1 = document.getElementById('pop-homia');
+  const btn1 = document.getElementById("project1");
+  btn1.onclick = function () {
+    targetDiv1.style.display = "block"
+  }
+
+  const targetDiv2 = document.getElementById('pop-scandy');
+  const btn2 = document.getElementById("project2");
+  btn2.onclick = function () {
+    targetDiv2.style.display = "block"
+  }
+
+  const targetDiv3 = document.getElementById('pop-portfolio');
+  const btn3 = document.getElementById("project3");
+  btn3.onclick = function () {
+    targetDiv3.style.display = "block"
+  }
+
+  const targetDiv4 = document.getElementById('pop-budget');
+  const btn4 = document.getElementById("project4");
+  btn4.onclick = function () {
+    targetDiv4.style.display = "block"
+  }
+
+
+    //close projects details
+
+    const closePop = document.getElementsByClassName('projects-pop');
+    const closeBtn = document.getElementsByClassName('close');
+    closeBtn.onclick = function () {
+      closePop.style.display = "none";
+    }
